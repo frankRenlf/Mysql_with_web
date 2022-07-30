a6b03f7770269553474172963152985e189dcf17
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-07-31 00:00:44 +0800

    Update tbl_book.sql

--- tbl_book.sql
+++ tbl_book.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 09/06/2022 18:01:34
+ Date: 30/07/2022 21:53:57
 */
 
 SET NAMES utf8mb4;
@@ -27,7 +27,7 @@ CREATE TABLE `tbl_book`  (
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 131 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tbl_book
@@ -36,14 +36,19 @@ INSERT INTO `tbl_book` VALUES (1, '计算机理论', 'Spring实战 第5版', 'Sp
 INSERT INTO `tbl_book` VALUES (2, '计算机理论', 'Spring 5核心原理与30个类手写实战', '十年沉淀之作，手写Spring精华思想');
 INSERT INTO `tbl_book` VALUES (3, '计算机理论', 'Spring 5设计模式', '全方位解析面向Web应用的轻量级框架，带你成为Spring MVC开发高手');
 INSERT INTO `tbl_book` VALUES (4, '计算机理论', 'Spring MVC+MyBatis开发从入门到项目实战', '源码级剖析Spring框架，适合已掌握Java基础的读者');
-INSERT INTO `tbl_book` VALUES (5, '计算机理论', '轻量级Java Web企业应用实战', '源码级剖析Spring框架，适合已掌握Java基础的读者');
 INSERT INTO `tbl_book` VALUES (6, '计算机理论', 'Java核心技术卷I基础知识(原书第11版)', 'Core Java第11版，Jolt大奖获奖作品，针对Java SE9、10、11全面更新');
 INSERT INTO `tbl_book` VALUES (7, '计算机理论', '深入理解Java虚拟机', '5个维度全面剖析JVM，大厂面试知识点全覆盖');
 INSERT INTO `tbl_book` VALUES (8, '计算机理论', 'Java编程思想(第4版)', 'Java学习必读经典,殿堂级著作!赢得了全球程序员的广泛赞誉');
 INSERT INTO `tbl_book` VALUES (9, '计算机理论', '零基础学Java(全彩版)', '零基础自学编程的入门图书，由浅入深，详解Java语言的编程思想和核心技术');
-INSERT INTO `tbl_book` VALUES (10, '市场营销', '直播就该这么做:主播高效沟通实战指南', '李子染、李佳琦、薇娅成长为网红的秘密都在书中');
-INSERT INTO `tbl_book` VALUES (11, '市场营销', '直播销讲实战—本通', '和秋叶—起学系列网络营销书籍');
-INSERT INTO `tbl_book` VALUES (12, '市场营销', '直播带货:淘宝、天猫直播从新手到高手', '—本教你如何玩转直播的书，10堂课轻松实现带货月入3W+');
-INSERT INTO `tbl_book` VALUES (13, 'science', 'test1', '1234566');
+INSERT INTO `tbl_book` VALUES (10, '计算机理论', '直播就该这么做:主播高效沟通实战指南', '李子染、李佳琦、薇娅成长为网红的秘密都在书中');
+INSERT INTO `tbl_book` VALUES (25, 'novel', 'novel1', '12345');
+INSERT INTO `tbl_book` VALUES (123, '123', '123', '123');
+INSERT INTO `tbl_book` VALUES (124, '123', '123', '123');
+INSERT INTO `tbl_book` VALUES (125, 'test1', '123', '123');
+INSERT INTO `tbl_book` VALUES (126, 'last', '2', '2');
+INSERT INTO `tbl_book` VALUES (127, 'science', 'test2', '156');
+INSERT INTO `tbl_book` VALUES (128, 'IServiceTest', 'byNow', '0012');
+INSERT INTO `tbl_book` VALUES (129, 'journal', 'testService', '8848');
+INSERT INTO `tbl_book` VALUES (130, 'journal', '2022_7_30', 'test before deploy\n');
 
 SET FOREIGN_KEY_CHECKS = 1;
