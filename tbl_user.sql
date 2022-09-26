a3935f685126c10ba9035b1a4ea2125009bf2da3
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-09-26 17:12:58 +0800

    Update tbl_user.sql

--- tbl_user.sql
+++ tbl_user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 13/06/2022 17:28:29
+ Date: 26/09/2022 16:48:56
 */
 
 SET NAMES utf8mb4;
@@ -26,15 +26,18 @@ CREATE TABLE `tbl_user`  (
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tbl_user
 -- ----------------------------
-INSERT INTO `tbl_user` VALUES (1, 'frank', '123456');
-INSERT INTO `tbl_user` VALUES (2, 'frank2', '156');
-INSERT INTO `tbl_user` VALUES (3, 'frank2', '156');
-INSERT INTO `tbl_user` VALUES (26, 'frank332', '156');
-INSERT INTO `tbl_user` VALUES (27, 'latesttest', '000000');
+INSERT INTO `tbl_user` VALUES (1, 'frank', '666');
+INSERT INTO `tbl_user` VALUES (34, 'test', 'welldone');
+INSERT INTO `tbl_user` VALUES (55, 'frank', '666');
+INSERT INTO `tbl_user` VALUES (56, 'lily', '123');
+INSERT INTO `tbl_user` VALUES (60, 'son2', '666');
+INSERT INTO `tbl_user` VALUES (62, 'son4', '666');
+INSERT INTO `tbl_user` VALUES (63, 'frank', '111');
+INSERT INTO `tbl_user` VALUES (64, 'Frank', '222');
 
 SET FOREIGN_KEY_CHECKS = 1;
