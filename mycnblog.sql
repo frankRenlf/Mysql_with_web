cef48ee0dd314727bf23f57c8a33a4e59f1b4463
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-10-24 10:22:22 +0800

    update

--- mycnblog.sql
+++ mycnblog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 20/10/2022 17:27:00
+ Date: 23/10/2022 11:10:00
 */
 
 SET NAMES utf8mb4;
@@ -31,16 +31,13 @@ CREATE TABLE `articleinfo`  (
   `rcount` int NOT NULL DEFAULT 1,
   `state` int NULL DEFAULT 1,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of articleinfo
 -- ----------------------------
-INSERT INTO `articleinfo` VALUES (1, 'Java', 'Java正⽂', '2022-10-05 15:24:49', '2022-10-05 15:24:49', 1, 1, 1);
 INSERT INTO `articleinfo` VALUES (2, 'test2', '123', '2022-10-06 16:43:51', '2022-10-06 16:43:51', 2, 1, 1);
-INSERT INTO `articleinfo` VALUES (3, 'test3', 'frank111', '2022-10-07 15:14:24', '2022-10-07 15:14:24', 2, 1, 1);
-INSERT INTO `articleinfo` VALUES (4, 'editortest', 'content1.valueOf()', '2022-10-20 16:59:59', '2022-10-20 16:59:59', 1, 1, 1);
-INSERT INTO `articleinfo` VALUES (5, 'lily\'s edit', '# 在这里写下一篇博客\nlily test\n', '2022-10-20 17:10:14', '2022-10-20 17:10:14', 2, 1, 1);
+INSERT INTO `articleinfo` VALUES (12, 'welldone', '# 在这里写下一篇博客\npost test', '2022-10-22 20:25:29', '2022-10-22 20:25:29', 1, 1, 1);
 
 -- ----------------------------
 -- Table structure for userinfo
@@ -48,14 +45,15 @@ INSERT INTO `articleinfo` VALUES (5, 'lily\'s edit', '# 在这里写下一篇博
 DROP TABLE IF EXISTS `userinfo`;
 CREATE TABLE `userinfo`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `photo` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
-  `createtime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatetime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `createtime` datetime NULL DEFAULT NULL,
+  `updatetime` datetime NULL DEFAULT NULL,
   `state` int NULL DEFAULT 1,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userinfo
@@ -65,7 +63,7 @@ INSERT INTO `userinfo` VALUES (2, 'lily', '123', '', '2021-12-06 17:10:48', '202
 INSERT INTO `userinfo` VALUES (4, 'testadd', '123', '', '2022-10-06 11:36:28', '2022-10-06 11:36:28', 1);
 INSERT INTO `userinfo` VALUES (10, 'qwe', '123', NULL, '2022-10-15 17:19:21', '2022-10-15 17:19:21', 1);
 INSERT INTO `userinfo` VALUES (11, 'cost', '22', NULL, '2022-10-20 16:21:54', '2022-10-20 16:21:54', 1);
-INSERT INTO `userinfo` VALUES (12, 'frank', '666', '', '2022-10-20 17:16:46', '2022-10-20 17:16:46', 1);
+INSERT INTO `userinfo` VALUES (17, 'frank2', '1', NULL, '2022-10-22 14:33:30', '2022-10-22 14:33:30', 1);
 
 -- ----------------------------
 -- Table structure for videoinfo
